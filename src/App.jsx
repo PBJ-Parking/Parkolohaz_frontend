d1e36091bc1046d3ea8b69726b62ae1496002982
PatrisaRose <cseresznyevirag4@gmail.com>
2024-03-02 17:23:51 +0100

    Alap project + header létrehozása

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <><main>
+      <header>
+        <h1>PBJ-Parking</h1>
+        <ul>
+          <li><a href=""><i className="bi bi-box-arrow-in-right"></i> Belépés</a></li>
+          <li><a href=""> <i className="bi bi-person-plus-fill"></i> Regisztáció</a></li>
+        </ul>
+      </header>
+    </main>
+    </>
+  )
+}
+
+export default App
