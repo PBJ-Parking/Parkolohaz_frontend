3d69ea567249c9f795fa6eba0dfd58a8c851aec9
terenyijanos96 <terenyi.janos96@gmail.com>
2024-03-02 18:08:06 +0100

    Article létrehozása

---
+++ src/components/Article.jsx
@@ -0,0 +1,25 @@
+import "./css/article.css"
+import Kepnezegeto from "./Kepnezegeto";
+
+
+export default function Article() {
+
+  return (
+    <article>
+      <h2>Üdvözöljük!</h2>
+
+      <Kepnezegeto />
+
+      <h3>Rólunk</h3>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore
+        maiores in distinctio exercitationem, vitae consectetur ipsa enim, omnis
+        at nobis eos quisquam eveniet qui? Veniam enim recusandae inventore
+        molestiae perspiciatis. Lorem, ipsum dolor sit amet consectetur
+        adipisicing elit. Voluptates reiciendis laboriosam fugit quia non, minus
+        soluta alias hic officiis id odit quo vel? Animi sit quaerat expedita
+        natus consequatur earum!
+      </p>
+    </article>
+  );
+}
