00160cf4f7e625b0e5848e32b05b97bd3220bbdb
terenyijanos96 <terenyi.janos96@gmail.com>
2024-04-08 16:52:03 +0200

    vite: css module bekonfigurálása + css module tesztelése a képnézegetővel

--- src/components/Kepnezegeto.jsx
+++ src/components/Kepnezegeto.jsx
@@ -1,4 +1,4 @@
-import "../css/kepnezegeto.css";
+import styles from  "../css/Kepnezegeto.module.css";
 
 import kep1 from "../../kepek/slide/kep1.webp";
 import kep2 from "../../kepek/slide/kep2.webp";
@@ -11,11 +11,11 @@ const keplista= [kep1, kep2, kep3, kep4, kep5, kep6]
 
 export default function Kepnezegeto() {
   return (
-    <Carousel className="slideshow">
+    <Carousel className={styles.slideshow}>
       {keplista.map((value, index) => {
         return (
           <Carousel.Item key={index}>
-            <img src={value} />
+            <img className={styles.slideshowImage} src={value} />
           </Carousel.Item>
         );
       })}
