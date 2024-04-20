427be3e1d4de7485fac691e4bad5db350a629d1d
BotkaB <botikabinaka@gmail.com>
2024-04-20 20:20:21 +0200

    statisztika formázás

--- src/pages/AdminStatisztika.jsx
+++ src/pages/AdminStatisztika.jsx
@@ -2,26 +2,35 @@
 
 import Card from 'react-bootstrap/Card';
 import { Link } from "react-router-dom";
+import { Container } from "react-bootstrap";
 import "../css/kapcsolat.css" 
 import "../css/fooldalArticle.css";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function AdminStatisztika(props) {
 
     return (
         <article>
-         
+    <Container fluid className="fooldal-container">
+   <Row>  
+   <Col>  
     <Card border="info"  bg="light" style={{ width: '18rem' }}>
     <Card.Header>Parkolóhelyek</Card.Header>
    <Card.Body> <Link to="/admin/statisztikak/parkolohely">Foglaltság adatai</Link></Card.Body>
    <Card.Body> <Link to="/admin/statisztikak/parkolohelyTipusok">Típusok szerinti megoszlás</Link></Card.Body>
     </Card>
-
+    </Col>
+    <Col>  
     <Card border="info"  bg="light" style={{ width: '18rem' }}>
     <Card.Header>Jarmuvek</Card.Header>
    
    <Card.Body> <Link to="/admin/statisztikak/jarmuTipusok">Típusok szerinti megoszlás</Link></Card.Body>
     </Card>
-    
+    </Col>
+    <Col></Col>
+    </Row> 
+    </Container>
         </article> 
     )
  
